0525ea63c58e68218cee2e4f5d637b02db014695
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-01-06 21:06:56 +0900

    view의 역할 : 화면에 보여주는 부분

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -1,31 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Topic extends CI_Controller { //java처럼 파일명과 매치
     function index(){ //약속
-        
-        echo '
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8"/>
-            </head>
-            <body>
-                토픽 메인 페이지
-            </body>
-        </html>
-        ';
+        $this->load->view('header');
+        $this->load->view('topic'); //view안의 topic을 찾아준다.
+        $this->load->view('footer');
     }
     function get($id){
-        echo '
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8"/>
-            </head>
-            <body>
-                토픽 '.$id.'
-            </body>
-        </html>
-        ';
+        $this->load->view('header');
+        $this->load->view('main',array('id'=>$id));
+        $this->load->view('footer');
     }
 }
 //주소/index.php/컨트롤러 안에 있는파일/메소드(post,get)/인자
